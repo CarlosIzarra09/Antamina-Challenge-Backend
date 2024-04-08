95cdd551e59125ce1759bd20f9ee6aca3eea2933
CarlosIzarra09 <eduizarra2@hotmail.com>
2024-04-07 23:21:48 -0500

    feat: Add summaryData class definition

--- src/app.py
+++ src/app.py
@@ -38,6 +38,26 @@ class Summary(db.Model):
 
 with app.app_context():
     db.create_all()
+    
+class SummaryData:
+    name_sensor: str
+    average_value: float
+    unit: str
+    timestamp: datetime
+
+    def __init__(self, name_sensor, average_value, unit, timestamp) -> None:
+        self.name_sensor = name_sensor
+        self.average_value = average_value
+        self.unit = unit
+        self.timestamp = timestamp
+    
+    def to_dict(self):
+        return {
+            'name_sensor': self.name_sensor,
+            'average_value': self.average_value,
+            'unit': self.unit,
+            'timestamp': self.timestamp
+        }
 
 class MeasurementDetailSchema(ma.Schema):
     class Meta:
