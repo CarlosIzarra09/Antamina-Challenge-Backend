796587b4efb3615fc24b0e078d908645ead4baf0
CarlosIzarra09 <eduizarra2@hotmail.com>
2024-04-07 20:26:56 -0500

    feat: Add alchemy config

--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, jsonify, request
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://root:password@127.0.0.1/sensors_db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
     
