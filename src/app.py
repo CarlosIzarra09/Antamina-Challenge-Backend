31a05d1f41ae34b68580c9992abef9a44a31e44e
CarlosIzarra09 <eduizarra2@hotmail.com>
2024-04-07 23:19:39 -0500

    feat: Add Entities MeasurementDetail and Summary

--- src/app.py
+++ src/app.py
@@ -11,7 +11,33 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+class MeasurementDetail(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name_sensor = db.Column(db.String(20))
+    value = db.Column(db.Float)
+    unit = db.Column(db.String(20))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    summary_id = db.Column(db.Integer, db.ForeignKey("summary.id"))
+    summary = db.relationship("Summary")
 
+    def __init__(self, name_sensor, value, unit, timestamp, summary_id):
+        self.name_sensor = name_sensor
+        self.value = value
+        self.unit = unit
+        self.timestamp = timestamp
+        self.summary_id = summary_id
+
+
+class Summary(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    data = db.Column(db.JSON)
+
+    def __init__(self, data):
+        self.data = data
+
+
+with app.app_context():
+    db.create_all()
     
     
 if __name__ == "__main__":
